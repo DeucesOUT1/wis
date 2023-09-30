31a9021d3ff312c5df175646ff5e58b452b8ff62
DeucesOUT1 <timmangoandre@gmail.com>
2023-09-30 14:48:24 +0800

    done did it

---
+++ 04_variable_types.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable Types</title>
+</head>
+<body>
+    <?php
+        $int_var = 12345;
+        print $int_var;
+        $another_int = -12345 + 12345;
+        $many = 2.2888800;
+        $many_2 = 2.2111200;
+        $few = $many + $many_2;
+        print "<br>";
+        print($many + $many_2 = $few);
+        print "<br> okurrr";
+        print "<br> ok na sir ";
+    ?>
+</body>
+</html>
